60c27694d8cbc6ea237e8b6e81064f29afb248c9
AVIKUMAR TALAVIYA <88608935+avikumart@users.noreply.github.com>
2022-06-05 15:38:14 +0530

    Update Stremlit_demo_webapp.py

--- Stremlit_demo_webapp.py
+++ Stremlit_demo_webapp.py
@@ -5,7 +5,8 @@ import numpy as np
 
 df = pd.DataFrame({"Corruption_INdex":[77, 53, 40, 47, 38, 75, 85, 81, 82, 81, 88, 90, 69, 74, 29, 62, 48,
        36, 44, 35]})
-#st.dataframe(df)
+
+st.dataframe(df)
 
 #plot a line chart
 st.line_chart(df)
