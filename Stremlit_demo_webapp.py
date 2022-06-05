61d34b5f728a43d3f31768decf36c9f00d9d3682
AVIKUMAR TALAVIYA <88608935+avikumart@users.noreply.github.com>
2022-06-05 13:36:59 +0530

    Update Stremlit_demo_webapp.py

--- Stremlit_demo_webapp.py
+++ Stremlit_demo_webapp.py
@@ -3,5 +3,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame({"Data":np.array(np.logspace(0,2,20))})
+df = pd.DataFrame({"Log_Data":np.array(np.logspace(0,2,20))})
 st.line_chart(df)
