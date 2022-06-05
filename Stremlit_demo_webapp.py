b0e3032e95898cbfecd9edfe37c5d775caecb9d2
AVIKUMAR TALAVIYA <88608935+avikumart@users.noreply.github.com>
2022-06-05 15:44:59 +0530

    Update Stremlit_demo_webapp.py

--- Stremlit_demo_webapp.py
+++ Stremlit_demo_webapp.py
@@ -6,9 +6,18 @@ import numpy as np
 df = pd.DataFrame({"Corruption_INdex":[77, 53, 40, 47, 38, 75, 85, 81, 82, 81, 88, 90, 69, 74, 29, 62, 48,
        36, 44, 35]})
 
-st.dataframe(df)
+st.title("Corruption index WebApp")
+st.write("This is the dataframe representing the below chart: ", pd.DataFrame({
+     'country_name': ['Hong Kong', 'South Korea', 'China', 'Italy', 'Mongolia',
+       'Austria', 'Norway', 'UK', 'Canada', 'Germany', 'Sweden',
+       'Denmark', 'France', 'United States', 'Russia ', 'Portugal',
+       'Romania', 'Argentina', 'Greece', 'Thailand '],
+     'corruption index': [77, 53, 40, 47, 38, 75, 85, 81, 82, 81, 88, 90, 69, 74, 29, 62, 48,
+       36, 44, 35],
+ }))
 
 #plot a line chart
-st.line_chart(df)
 st.title("Corruption Index by countries")
+st.line_chart(df)
+
 
