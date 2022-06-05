9be27d9815d173b5dd92b5abb55eab968892ad27
AVIKUMAR TALAVIYA <88608935+avikumart@users.noreply.github.com>
2022-06-05 13:31:44 +0530

    Update Stremlit_demo_webapp.py

--- Stremlit_demo_webapp.py
+++ Stremlit_demo_webapp.py
@@ -3,6 +3,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("country.csv")
-
-st.line_chart(df['Corruption_Index'])
+df = pd.DataFrame({"Data":np.array(np.logspace(0,2,20))})
+st.line_chart(df)
