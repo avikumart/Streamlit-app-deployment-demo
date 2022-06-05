5bf4cc771c91d404156dba58a0e482f17dfb9296
AVIKUMAR TALAVIYA <88608935+avikumart@users.noreply.github.com>
2022-06-05 15:37:01 +0530

    Update Stremlit_demo_webapp.py

--- Stremlit_demo_webapp.py
+++ Stremlit_demo_webapp.py
@@ -3,16 +3,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame({"Country":['Hong Kong', 'South Korea', 'China', 'Italy', 'Mongolia',
-       'Austria', 'Norway', 'UK', 'Canada', 'Germany', 'Sweden',
-       'Denmark', 'France', 'United States', 'Russia ', 'Portugal',
-       'Romania', 'Argentina', 'Greece', 'Thailand '],
-                   "Corruption_INdex":[77, 53, 40, 47, 38, 75, 85, 81, 82, 81, 88, 90, 69, 74, 29, 62, 48,
-       36, 44, 35]
-       })
+df = pd.DataFrame({"Corruption_INdex":[77, 53, 40, 47, 38, 75, 85, 81, 82, 81, 88, 90, 69, 74, 29, 62, 48,
+       36, 44, 35]})
 #st.dataframe(df)
 
 #plot a line chart
-st.bar_chart(df)
+st.line_chart(df)
 st.title("Corruption Index by countries")
 
