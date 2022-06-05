6d5ce7473be010ad69bf9d0ebf56e5511c9ce837
AVIKUMAR TALAVIYA <88608935+avikumart@users.noreply.github.com>
2022-06-05 15:32:26 +0530

    Update Stremlit_demo_webapp.py

--- Stremlit_demo_webapp.py
+++ Stremlit_demo_webapp.py
@@ -10,10 +10,9 @@ df = pd.DataFrame({"Country":['Hong Kong', 'South Korea', 'China', 'Italy', 'Mon
                    "Corruption_INdex":[77, 53, 40, 47, 38, 75, 85, 81, 82, 81, 88, 90, 69, 74, 29, 62, 48,
        36, 44, 35]
        })
-st.dataframe(df)
+#st.dataframe(df)
 
 #plot a line chart
-
 st.line_chart(df)
 st.title("Corruption Index by countries")
 
